267ef85f2bdc771dd789b6e158d6a0544886d0aa
ak <kevin-yhx@hotmail.com>
2020-07-12 13:57:45 +0800

    Learning pySpark

---
+++ Python/Sandbox/pySpark_wordCount1.py
@@ -0,0 +1,34 @@
+﻿#!/usr/local/bin/python
+# -*- coding:utf-8 -*-
+ 
+import sys
+from pyspark.sql import SparkSession
+from operator import add
+#file=sys.args[1]
+
+def WordCount1(file):
+	"""
+	reduceByKey, (k,v) only
+	build record as (record,1) and do add/+
+	if using add, need 
+	from operator import add
+	"""
+	
+	ss=SparkSession.builder.appName('wordCount1').config('spark.default.parallelism',24).getOrCreate()
+	ual=ss.read.text(sys.argv[1]).rdd
+	ual=ual.map(lambda l:l[0]).map(lambda x:x.split("\t")).map(lambda x:(x[7],x[16])).flatMap(lambda x:x)
+
+	#ual.take(30)
+
+	wc=ual.map(lambda x:(x,1)).reduceByKey(lambda a,b:a+b).sortBy(lambda x:int(x[1]),False).collect()
+
+	print "Word:\t\t\tCounts"
+	for j,i in wc:
+		print "%s:\t\t\t%d" % (j,i)
+	ss.stop()
+	
+if __name__ ==  "__main__":
+	if (len(sys.argv)!=2):
+		print("Usage: %s <file>" % sys.argv[0])
+		exit(-1)
+	WordCount1(sys.argv[1])
---
+++ Python/Sandbox/pySpark_wordCount2.py
@@ -0,0 +1,28 @@
+#!usr/local/bin/python
+# -*- coding:utf-8 -*-
+
+import sys
+from pyspark.sql import SparkSession
+from operator import add
+
+def wordCountFunc(file):
+	'''
+	reduceByKey() = groupByKey().mapValues()
+	groupByKey just combine all values together without doing anything
+	
+	from pyspark.sql import SparkSession
+	'''
+	ss=SparkSession.builder.appName("wordCountUsing").config('spark.default.parallelism',20).getOrCreate()
+	webtr=ss.read.option("header","true").csv(sys.argv[1]).rdd.map(lambda x:(x[2],1))
+	
+	wt=webtr.groupByKey().mapValues(lambda x:len(x)).sortBy(lambda x:x[1],False).collect()
+	print "Word\t\t\tCount"
+	for i,j in wt:
+		print("%s\t\t\t%d" % (i,j))
+	ss.stop()
+
+if __name__ == "__main__":
+	if(len(sys.argv)!=2):
+		print("Missing parameter: <file>. Usage: %s <file>" % (sys.argv[0]))
+		exit(-1)
+	wordCountFunc(sys.argv[1])
